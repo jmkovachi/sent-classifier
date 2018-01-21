4271e41c517fba055b1a46dbaeb725c64547eb2b
John Michael Kovachi <johnm.kovachi@gmail.com>
2018-01-21 14:07:32 -0500

    Cleaned up and commented

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -6,14 +6,19 @@ Created on Sat Jan 20 19:27:44 2018
 @author: jmkovachi
 """
 
-from read_MPQA import extract_MPQA as MPQA
 import math
+from read_MPQA import extract_MPQA as MPQA
 from nltk.stem import WordNetLemmatizer
 
+
 class MN_NaiveBayes:
-    
+
     """
-    Constructor for MN_NaiveBayes. 
+    Constructor for MN_NaiveBayes.
+    Initializes overall counts of positive, negative, and neutral classes.
+    Initializes overall document count for use in a priori class probability
+    calculation.
+    Initializes pos, neg, and neutral feature count dictionaries.
     """
     def __init__(self, pos, neg, neutral):
         self.pos_count = MPQA.count(pos)
@@ -23,9 +28,9 @@ class MN_NaiveBayes:
         self.pos = pos
         self.neg = neg
         self.neutral = neutral
-    
+
     """
-    An implementation of Jurafsky's MN Bayes Network 
+    An implementation of Jurafsky's MN Bayes Network
     algorithm.
     """
     def train(self):
@@ -33,31 +38,50 @@ class MN_NaiveBayes:
         self.features['posFeatures'] = {}
         self.features['negFeatures'] = {}
         self.features['neutralFeatures'] = {}
-        
+
+        # Gathering a priori probabilities by class
         self.priorLogPos = math.log(self.pos_count/self.doc_count)
         self.priorLogNeg = math.log(self.neg_count/self.doc_count)
         self.priorLogNeutral = math.log(self.neu_count/self.doc_count)
-        
+ 
+        """
+        Each for loop below is calculating probabilities of each feature
+        for each class.
+        Backslashes in calculations are added for readiblity and serve as 
+        line breaks.
+        """
         for word, count in self.pos.items():
-            self.features['posFeatures'][word] = math.log((count + 1)
+            self.features['posFeatures'][word] = math.log((count + 1) \
                                             /(self.pos_count + self.doc_count))
         for word, count in self.neg.items():
-            self.features['negFeatures'][word] = math.log((count + 1)
+            self.features['negFeatures'][word] = math.log((count + 1) \
                                             /(self.neg_count + self.doc_count))
-        
+
         for word, count in self.neutral.items():
-            self.features['neutralFeatures'][word] = math.log((count + 1)
+            self.features['neutralFeatures'][word] = math.log((count + 1) \
                                             /(self.neu_count + self.doc_count))
-        
-    def test(self,document):
+
+    """
+    Takes a given test document and make a classification decision based off
+    of a max probability.
+    @param document: Test document used to make classification decision.
+    return: A two-tuple with the classification decision and its corresponding
+    log-space probability.
+    """
+    def test(self, document):
         wordnet_lemmatizer = WordNetLemmatizer()
         document = [wordnet_lemmatizer.lemmatize(x) for x in document.split(" ")]
         pos_val = self.priorLogPos
-        smooth_pos = math.log(1/(self.pos_count + self.doc_count))
         neg_val = self.priorLogNeg
-        smooth_neg = math.log(1/(self.neg_count + self.doc_count))
         neutral_val = self.priorLogNeutral
+        
+        # Smoothed probabilities are calculated below, these are used when a 
+        # word in the test document is not found in the given class but is found
+        # in another class's feature dict
+        smooth_pos = math.log(1/(self.pos_count + self.doc_count))
+        smooth_neg = math.log(1/(self.neg_count + self.doc_count))
         smooth_neutral = math.log(1/(self.neu_count + self.doc_count))
+        
         for feature in self.features:
             if feature == 'posFeatures':
                 for word in document:
@@ -87,14 +111,14 @@ class MN_NaiveBayes:
             return ('neutral', neutral_val)
         else:
             return ('positive', pos_val)
-                    
+                  
 mpqa = MPQA()
 
 pol_list, sent_list = mpqa.build_BOW(file_path='/home/jmkovachi/sent-classifier/database.mpqa.3.0/gate_anns')
 
 pos, neg, neutral = mpqa.build_counts(pol_list, sent_list)
-  
-print(neg)      
+
+print(neg)   
 NB = MN_NaiveBayes(pos, neg, neutral)
 
 NB.train()
