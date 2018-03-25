9e968ac2a15416685afa0cdd9e9222c2bf73b570
John Michael Kovachi <johnm.kovachi@gmail.com>
2018-03-25 15:25:35 -0400

    Remove class and just add main method

--- indexES.py
+++ indexES.py
@@ -4,24 +4,9 @@ import time
 import os
 es = Elasticsearch()
 
-class IndexES:
-
-    def extract_header(text):
-        """
-        Extracts the header and text from a given article. 
-        @param text: Article text to be extracted
-        return: title, author, date, link to article, and body of text
-        """
-        search = re.search(r'--(.+?)--(.+?)--(.+?)--(.+?)Reuters\)\s-', text, flags=re.DOTALL)
-        text = re.sub(r'--.+?--.+?--.+?--.+?Reuters\)\s-', '', text)
-        title = search.group(1)
-        author = search.group(2)
-        date = search.group(3)
-        link = search.group(4)
-        return title, author, date, link, text
 
 
-    def correct_date_format(string):
+def correct_date_format(string):
         """
         Gives a correct date format to be entered into the database. This date format is
         compatible with the Quandl API and can be passed directly as a parameter.
@@ -41,22 +26,41 @@ class IndexES:
         t = time.strftime('%Y-%m-%d-%yT%Hh%Mm%S', t)
         return t
 
-    def read_and_index():
-        """
-        Reads files and subsequently indexes them into an ElasticSearch database. Used for easily querying needed articles.
-        """
 
-        path = os.getcwd() 
-        reuters_folders = os.listdir('/home/jmkovachi/Documents/jupyter_notebooks/reuters')[0:10]
-        path += '/reuters'
-        articles = []
-        id = 1
-        for folder in reuters_folders:
-            article_files = os.listdir(path + '/' + folder)
-            for file in article_files:
-                with open(path + '/' + folder + '/' + file) as f:
-                    raw_text = f.read()
-                    title, author, date, link, text = extract_header(raw_text)
-                    es.index(index='articles', doc_type='article', id=id, body={'title' : title.replace('\n',''), 'author' : author.replace('\n',''), 'date' : date.replace('\n',''), 'link' : link.replace('\n',''), 'text' : text.replace('\n','')})
-                    id += 1
+def extract_header(text):
+    """
+    Extracts the header and text from a given article. 
+    @param text: Article text to be extracted
+    return: title, author, date, link to article, and body of text
+    """
+    search = re.search(r'--(.+?)--(.+?)--(.+?)--(.+?)Reuters\)\s-', text, flags=re.DOTALL)
+    text = re.sub(r'--.+?--.+?--.+?--.+?Reuters\)\s-', '', text)
+    title = search.group(1)
+    author = search.group(2)
+    date = correct_date_format(search.group(3))
+    link = search.group(4)
+    return title, author, date, link, text
 
+
+def read_and_index():
+    """
+    Reads files and subsequently indexes them into an ElasticSearch database. Used for easily querying needed articles.
+    """
+
+    path = os.getcwd() 
+    reuters_folders = os.listdir('/home/jmkovachi/Documents/jupyter_notebooks/reuters')[0:10]
+    path += '/reuters'
+    articles = []
+    id = 1
+    for folder in reuters_folders:
+        article_files = os.listdir(path + '/' + folder)
+        for file in article_files:
+            with open(path + '/' + folder + '/' + file) as f:
+                raw_text = f.read()
+                title, author, date, link, text = extract_header(raw_text)
+                es.index(index='articles', doc_type='article', id=id, body={'title' : title.replace('\n',''), 'author' : author.replace('\n',''), 'date' : date.replace('\n',''), 'link' : link.replace('\n',''), 'text' : text.replace('\n','')})
+                id += 1
+
+
+if __name__ == "__main__":
+    read_and_index()
