b70368049050a5ddbfc668cdbfb6bca3e6807f45
John Michael Kovachi <johnm.kovachi@gmail.com>
2018-03-25 16:15:11 -0400

    Clean up file parsing

--- indexES.py
+++ indexES.py
@@ -2,29 +2,31 @@ import re
 from elasticsearch import Elasticsearch
 import time
 import os
+import codecs
 es = Elasticsearch()
 
 
 
 def correct_date_format(string):
-        """
-        Gives a correct date format to be entered into the database. This date format is
-        compatible with the Quandl API and can be passed directly as a parameter.
-        @param string: Date string to be converted
-        return: converted time
+    """
+    Gives a correct date format to be entered into the database. This date format is
+    compatible with the Quandl API and can be passed directly as a parameter.
+    @param string: Date string to be converted
+    return: converted time
 
-        Example input and output is below.
+    Example input and output is below.
 
-        Mon Jan 5, 2009 7:53pm EST
+    Input: Mon Jan 5, 2009 7:53pm EST
 
-        2018-03-17T01h23m24
+    Return: 2009-01-05T07h53m00
 
-        """
-        string = re.sub(r'\s(\d),', r' 0\1', string)
-        string = re.sub(r'(\d):(\d\d)', r'0\1:\2', string)
-        t = time.strptime(string, '%a %b %d %Y %H:%M%p %Z')
-        t = time.strftime('%Y-%m-%d-%yT%Hh%Mm%S', t)
-        return t
+    """
+    print(string)
+    string = re.sub(r'\s(\d),', r' 0\1', string)
+    string = re.sub(r'\s(\d):(\d\d)', r' 0\1:\2', string)
+    t = time.strptime(string, '%a %b %d %Y %H:%M%p %Z')
+    t = time.strftime('%Y-%m-%dT%Hh%Mm%S', t)
+    return t
 
 
 def extract_header(text):
@@ -33,11 +35,11 @@ def extract_header(text):
     @param text: Article text to be extracted
     return: title, author, date, link to article, and body of text
     """
-    search = re.search(r'--(.+?)--(.+?)--(.+?)--(.+?)Reuters\)\s-', text, flags=re.DOTALL)
-    text = re.sub(r'--.+?--.+?--.+?--.+?Reuters\)\s-', '', text)
+    search = re.search('--(.+?)\n--(.+?)\n--(.+?)\n--(.+?)\n.+?Reuters\)\s-', text, flags=re.DOTALL)
+    text = re.sub('--.+?--.+?--.+?--.+?Reuters\)\s-', '', text, flags=re.DOTALL)
     title = search.group(1)
-    author = search.group(2)
-    date = correct_date_format(search.group(3))
+    author = re.sub('By\s', '', search.group(2))
+    date = correct_date_format(codecs.getdecoder('unicode_escape')(search.group(3))[0].replace('\n','').strip())
     link = search.group(4)
     return title, author, date, link, text
 
@@ -46,11 +48,9 @@ def read_and_index():
     """
     Reads files and subsequently indexes them into an ElasticSearch database. Used for easily querying needed articles.
     """
-
     path = os.getcwd() 
-    reuters_folders = os.listdir('/home/jmkovachi/Documents/jupyter_notebooks/reuters')[0:10]
+    reuters_folders = os.listdir('/home/jmkovachi/Documents/jupyter_notebooks/reuters')
     path += '/reuters'
-    articles = []
     id = 1
     for folder in reuters_folders:
         article_files = os.listdir(path + '/' + folder)
@@ -58,7 +58,11 @@ def read_and_index():
             with open(path + '/' + folder + '/' + file) as f:
                 raw_text = f.read()
                 title, author, date, link, text = extract_header(raw_text)
-                es.index(index='articles', doc_type='article', id=id, body={'title' : title.replace('\n',''), 'author' : author.replace('\n',''), 'date' : date.replace('\n',''), 'link' : link.replace('\n',''), 'text' : text.replace('\n','')})
+                es.index(index='articles', doc_type='article' id=id, body={'title' : codecs.getdecoder('unicode_escape')(title)[0].replace('\n','').strip(),
+                                                                            'author' : author.replace('\n','').strip(), 
+                                                                            'date' : date,
+                                                                            'link' : link.replace('\n','').strip(),
+                                                                            'text' : codecs.getdecoder('unicode_escape')(text)[0].replace('\n','').strip()})
                 id += 1
 
 
