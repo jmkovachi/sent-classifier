1b4b3e4d8ab77d5071701e30f0d3ac348e4c0fb3
John Michael Kovachi <johnm.kovachi@gmail.com>
2018-03-26 14:48:21 -0400

    Begin refactoring Reuters_PMI.py to conform to an OO structure

--- Reuters_PMI.py
+++ Reuters_PMI.py
@@ -1,17 +1,20 @@
-
-# coding: utf-8
-
-# In[4]:
-
+from xlrd import open_workbook
+import re
+import nltk
+import math
+import pandas as pd
+import numpy as np
 
 class McDonald_Word_List:
     def __init__(self, pos_words, neg_words):
-        self.pos_words = pos_words
-        self.neg_words = neg_words
-        self.pos_word_counts = {word:0 for word, val in pos_words.items()}
-        self.intersection_pos = {word:0 for word, val in pos_words.items()}
-        self.neg_word_counts = {word:0 for word, val in neg_words.items()}
-        self.intersection_neg = {word:0 for word,val in neg_words.items()}
+        self.pos_words = {}
+        self.neg_words = {}
+        self.pos_word_counts = {word:0 for word, val in self.pos_words.items()}
+        self.intersection_pos = {word:0 for word, val in self.pos_words.items()}
+        self.neg_word_counts = {word:0 for word, val in self.neg_words.items()}
+        self.intersection_neg = {word:0 for word,val in self.neg_words.items()}
+        self.pos_df = None
+        self.neg_df = None
         
     def __str__(self):
         print("""Pos words: {0}
@@ -19,177 +22,59 @@ class McDonald_Word_List:
                   len(self.pos_words),
                   len(self.neg_words)))
 
+    def getWB(self):
+        FORMAT = ['Positive', 'Negative']
+        values = ""
+
+        wb = open_workbook('McDonaldDict.xlsx')
+        print('Getting polarity lists...')
+        values = []
+        for s in wb.sheets():
+            words = []
+            for row in range(1, s.nrows):
+                col_names = s.row(0)[1:]
+                col_value = []
+                word = s.cell(row, 0).value
+                for name, col in zip(col_names, range(1,s.ncols)):
+                    value = (s.cell(row,col).value)
+                    if name.value == 'Positive' and int(value) > 0:
+                        self.pos_words[word] = int(value)
+                    elif name.value == 'Negative' and int(value) > 0:
+                        self.neg_words[word] = int(value)
+                    col_value.append((name.value, value))
+                values.append(col_value)
+
+    def num_words(self, sentences):
+        l = 0
+        pos_count = 0
+        neg_count = 0
+        for s in sentences:
+            l += len(s)
+            for word in nltk.word_tokenize(s):
+                if word.upper() in self.pos_words:
+                    pos_count += 1
+                elif word.upper() in self.neg_words:
+                    neg_count += 1
+        return l, pos_count, neg_count
+
+    @staticmethod
+    def extract_header(text):
+        search = re.search('--(.+?)--(.+?)--(.+?)--(.+?)Reuters\)\s-', text, flags=re.DOTALL)
+        text = re.sub('--.+?--.+?--.+?--.+?Reuters\)\s-', '', text)
+        title = search.group(1)
+        author = search.group(2)
+        date = search.group(3)
+        link = search.group(4)
+        return title, author, date, link, text
+
+    @staticmethod
+    def compute_PMI(class1, class2, int_c1c2, overall_count):
+        return math.log((int_c1c2+1/overall_count)/((class1+1/overall_count)*(class2+1/overall_count)))
+        # +1s added for smoothing
 
-# We will now read Bill McDonald's Excel file containing the master dictionary of financial sentiment words.
-# For this task, I am using the xlrd library. For now, I am only reading the cell values that have words with positive or negative sentiment.
 
-# In[5]:
 
 
-from xlrd import open_workbook
-FORMAT = ['Positive', 'Negative']
-values = ""
-
-wb = open_workbook('McDonaldDict.xlsx')
-print('Getting polarity lists...')
-values = []
-for s in wb.sheets():
-    #print 'Sheet:',s.name
-    words = []
-    pos = {}
-    neg = {}
-    for row in range(1, s.nrows):
-        col_names = s.row(0)[1:]
-        col_value = []
-        word = s.cell(row, 0).value
-        for name, col in zip(col_names, range(1,s.ncols)):
-            value = (s.cell(row,col).value)
-            if name.value == 'Positive' and int(value) > 0:
-                pos[word] = int(value)
-            elif name.value == 'Negative' and int(value) > 0:
-                neg[word] = int(value)
-            col_value.append((name.value, value))
-        values.append(col_value)
-mcd = McDonald_Word_List(pos, neg)
-print(mcd.pos_words)
-
-
-# Voila. We have our lists of positive and negative words generated. These words were annotated for the financial domain and will be what we use to analyze our pointwise mutual information across the corpus.
-
-# In[5]:
-
-
-import re
-
-
-# We will use the method below to extract header information from the news article. Additionally, we get rid of the miscellaneous header text in order to extract text more precisely.
-
-# In[6]:
-
-
-def extract_header(text):
-    print('hi')
-    search = re.search('--(.+?)--(.+?)--(.+?)--(.+?)Reuters\)\s-', text, flags=re.DOTALL)
-    text = re.sub('--.+?--.+?--.+?--.+?Reuters\)\s-', '', text)
-    title = search.group(1)
-    author = search.group(2)
-    date = search.group(3)
-    link = search.group(4)
-    return title, author, date, link, text
-
-
-# In[9]:
-
-
-import os
-
-path = os.getcwd() 
-
-reuters_folders = os.listdir('/home/jmkovachi/Documents/jupyter_notebooks/reuters')[0:10]
-
-path += '/reuters'
-
-articles = []
-
-
-# In[10]:
-
-
-print(reuters_folders)
-
-
-# In[ ]:
-
-
-for folder in reuters_folders:
-    article_files = os.listdir(path + '/' + folder)
-    for file in article_files:
-        with open(path + '/' + folder + '/' + file) as f:
-            raw_text = f.read()
-            title, author, date, link, text = extract_header(raw_text)
-            articles.append({title : title, author : author, date : date, link : link, text : text})
-
-
-
-# We use this code above to open up our Reuters folder and read the files from our directory. The data being used here comes from this repository [financial news corpus](https://github.com/philipperemy/financial-news-dataset). It is pretty great. 
-
-# In[9]:
-
-
-import nltk
-
-
-# In[10]:
-
-
-from nltk.chunk import conlltags2tree, tree2conlltags
-
-sentence = "Mark and John are working at Google."
-
-for sent in nltk.sent_tokenize(sentence):
-   for chunk in nltk.ne_chunk(nltk.pos_tag(nltk.word_tokenize(sent))):
-      if hasattr(chunk, 'label'):
-         print(chunk.label(), ' '.join(c[0] for c in chunk))
-
-
-# Above is some example code from nltk's NE chunker/tagger. It works quite well in our purposes for this PMI task.
-
-# Below is where I get into the meat of calculating the PMI. 
-# 
-# $$pmi(x,y) = log\frac{p(x,y)}{p(x)p(y)}$$
-# 
-# Usually we define p(x,y) as the probability of the intersection of two entities within some window. For the purposes of this experiment, I am defining windows as sentences. Therefore, the equation we arrive for calculating PMI at is:
-# 
-# $$pmi(x,y) = log\frac{count(x,y)_{D}}{count(x)_{D}count(y)_{D}}$$
-# 
-# Where $$D$$ is all of the documents in the Reuters corpus. $$x$$ and $$y$$ are occurrences of a polarity word (positive when calculating positive PMI, negative words when calculating negative PMI). 
-# 
-# Each article is looped through in order to build the overall counts of words in order to count PMIs.
-# 
-# Additionally, we store the counts of all words as they relate to organizations.
-
-# In[ ]:
-
-
-print(mcd.pos_words)
-
-
-# In[ ]:
-
-
-import math
-
-
-# In[ ]:
-
-
-def num_words(sentences):
-    l = 0
-    pos_count = 0
-    neg_count = 0
-    for s in sentences:
-        l += len(s)
-        for word in nltk.word_tokenize(s):
-            if word.upper() in mcd.pos_words:
-                pos_count += 1
-            elif word.upper() in mcd.neg_words:
-                neg_count += 1
-    return l, pos_count, neg_count
-
-
-# In[ ]:
-
-
-def compute_PMI(class1, class2, int_c1c2, overall_count):
-    return math.log((int_c1c2+1/overall_count)/((class1+1/overall_count)*(class2+1/overall_count)))
-    # +1s added for smoothing
-
-
-# In[ ]:
-
-
-import pandas as pd
-import numpy as np
 
 length = 0
 overall_pos = 0
@@ -200,11 +85,6 @@ intersection_neg = 0
 
 pos_df = pd.DataFrame(0, index=[str(key) for (key,val) in mcd.pos_words.items()], columns=[])
 neg_df = pd.DataFrame(0, index=[str(key) for (key,val) in mcd.neg_words.items()], columns=[])
-
-
-# These pandas dataframes will allow us to represent co-occurences of organizations and polarity words as a matrix. Initially, dataframes are initialized as 0 column matrices with row indexes as the words from the positive and negative financial polarity lists.
-
-# In[ ]:
 
 
 for article in articles[:1000]:
@@ -255,44 +135,11 @@ for article in articles[:1000]:
                 mcd.neg_word_counts[str(chunk[0]).upper()] += 1
        intersection_pos += org_count if org_count < pos_count else pos_count
        intersection_neg += org_count if org_count < neg_count else neg_count
-    #print(pos_count)
-    #print(overall_org)
-    #print(intersection)
-    #print(l)
     
 print(compute_PMI(overall_pos, overall_org, intersection_pos, l))
 print(compute_PMI(overall_neg, overall_org, intersection_neg, l))
 #print(mcd.pos_word_counts)
 print(pos_df)
-
-
-            
-                
-        
-              #print(chunk.label(), ' '.join(c[0] for c in chunk))
-    #create_co_occurrence_matrix(sentences)
-    
-
-
-# %%latex
-# 
-# Here is the above algorithm:
-# 
-# \begin{enumerate}
-#     \item Initialize two empty Pandas dataframes, one for the positive words and one for the negative words.
-#     \item Loop through each article:
-#     \begin{enumerate}
-#         \item Tokenize each sentence in the article using NLTK.
-#             \begin{enumerate}
-#                 \item Initialize an organization word count, a positive word count, a negative word count, and an empty list of orgs.
-#                 \item Chunk the sentence using the NLTK NER chunker. Loop through each chunk and append the organization to the org list. If the organization is not present in the columns of the dataframe, insert a new column into the dataframe. 
-#                 \item Loop through the chunks a second time. If the chunked word is in the positive (or negative, conversely) words dict, create a temporary organization list and increment the counts of the positive (negative) count (representing the number of co-occurrences in a sentence) and increment the index in the pandas dataframe corresponding to that positive or negative word.
-#             \end{enumerate}
-#     \end{enumerate}
-# \end{enumerate}
-
-# In[ ]:
-
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -323,9 +170,6 @@ plt.bar(range(len(sorted_counts[0:20])), PMIs, align='edge', width=.3)
 plt.xticks(range(len(sorted_counts[0:20])), [val[0] for val in sorted_counts[:20]])
 plt.xticks(rotation=70)
 plt.show()
-
-
-# In[ ]:
 
 
 
